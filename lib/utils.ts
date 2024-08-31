77f000ea348eb9f56b4b5fb4d1d380443961d254
AdanSerrano <adan.serrano@utp.ac.pa>
2024-08-31 10:00:26 -0500

    feat(setup blog page): :sparkles: components post-items to formated date added

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { posts } from '#site/content'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })
+}
+
+export function sortPosts(post: Array<posts>) {
+  return post.sort((a, b) => {
+    if (a.date > b.date) return -1;
+    if (a.date < b.date) return 1;
+    return 0;
+  })
 }
